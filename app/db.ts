b7c19bf6ff8d66b15afa531c39a37c4eb315c2da
Sooraj <sooraj.s@BigFormula.com>
2025-01-20 13:13:28 +0530

    env prod and db

--- app/db.ts
+++ app/db.ts
@@ -1,14 +1,31 @@
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import { Client } from 'pg';
+
+// const client = new Client({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// client.connect().catch((err: any) => console.error('Database connection failed', err));
+
+// export default client;
+
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Client } from 'pg';
 
+// Use the DATABASE_URL environment variable for the connection string
 const client = new Client({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Ensures SSL is handled correctly for production
+  },
 });
 
 client.connect().catch((err: any) => console.error('Database connection failed', err));
 
 export default client;
+
